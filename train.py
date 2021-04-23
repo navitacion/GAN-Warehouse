4ae08e23f320722f1cceec470e3ff7c42f7f1b51
navitacion <masataka.i.navi@gmail.com>
2021-04-23 11:05:06 +0900

    add:modelcheckpoint

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 import os
 import glob
+import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import CometLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
 import hydra
 from omegaconf import DictConfig
 from dotenv import load_dotenv
@@ -37,6 +39,8 @@ def main(cfg: DictConfig):
     # Lightning Module  ---------------------------------------------------------
     dm = None
     model = None
+    checkpoint_path = 'checkpoints/'
+    checkpoint_callback = ModelCheckpoint(dirpath=checkpoint_path, filename='{epoch:02d}', prefix=cfg.train.model, period=1)
 
     if cfg.train.model == 'vae':
         data_dir = 'data/'
@@ -47,31 +51,35 @@ def main(cfg: DictConfig):
         data_dir = 'data/'
         dm = CelebAHQDataModule(data_dir, transform, cfg)
         cfg.train.img_size = 128
-        model = DCGAN_LightningSystem(nets[0], nets[1], cfg)
+        model = DCGAN_LightningSystem(nets[0], nets[1], cfg, checkpoint_path)
 
     elif cfg.train.model == 'wgan_gp':
+        logger.log_hyperparams(dict(cfg.wgan_gp))
         data_dir = 'data/'
         dm = CelebAHQDataModule(data_dir, transform, cfg)
-        model = WGAN_GP_LightningSystem(nets[0], nets[1], cfg)
+        model = WGAN_GP_LightningSystem(nets[0], nets[1], cfg, checkpoint_path)
 
     elif cfg.train.model == 'cyclegan':
+        logger.log_hyperparams(dict(cfg.cyclegan))
         data_dir = 'data/'
         base_img_paths = glob.glob(os.path.join(data_dir, 'celeba_hq', '**/*.jpg'), recursive=True)
         style_img_paths = glob.glob(os.path.join(data_dir, 'van_gogh_paintings', '**/*.jpg'), recursive=True)
         dm = CycleGANDataModule(base_img_paths, style_img_paths, transform, cfg, phase='train', seed=cfg.train.seed)
         model = CycleGAN_LightningSystem(nets[0], nets[1], nets[2], nets[3],
-                                         transform, cfg=cfg)
+                                         transform, cfg, checkpoint_path)
 
     elif cfg.train.model == 'sagan':
+        logger.log_hyperparams(dict(cfg.sagan))
         data_dir = 'data/'
         dm = CelebAHQDataModule(data_dir, transform, cfg)
-        model = SAGAN_LightningSystem(nets[0], nets[1], cfg)
+        model = SAGAN_LightningSystem(nets[0], nets[1], cfg, checkpoint_path)
 
     # Trainer  ---------------------------------------------------------
     trainer = Trainer(
         logger=logger,
         max_epochs=cfg.train.epoch,
         gpus=1,
+        callbacks=[checkpoint_callback],
         # resume_from_checkpoint='./checkpoints/epoch=30.ckpt'
     )
 
