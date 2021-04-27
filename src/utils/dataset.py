e1758a2e3e954d7312f1184efe4ae3f00287dc6f
navitacion <masataka.i.navi@gmail.com>
2021-04-27 09:40:48 +0900

    modify:refactor

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,13 +1,10 @@
-import os
-import glob
 import cv2
 import numpy as np
-from PIL import Image
 from torch.utils.data import Dataset
 
 
 class SingleImageDataset(Dataset):
-    def __init__(self, img_paths, transform, phase='train'):
+    def __init__(self, img_paths, transform=None, phase='train'):
         self.transform = transform
         self.phase = phase
         self.img_paths = img_paths
@@ -18,35 +15,16 @@ class SingleImageDataset(Dataset):
     def __getitem__(self, idx):
         img_path = self.img_paths[idx]
         img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.uint8)
-        img = self.transform(img, self.phase)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if self.transform is not None:
+            img = self.transform(img, self.phase)
 
         return img
 
 
 
-# CelebA-HQ Dataset ---------------------------------------------------------------------------
-class CelebHQDataset(Dataset):
-    def __init__(self, data_dir, transform, phase='train'):
-        self.data_dir = data_dir
-        self.transform = transform
-        self.phase = phase
-        self.img_paths = glob.glob(os.path.join(self.data_dir, 'celeba_hq', '**/*.jpg'), recursive=True)
-
-    def __len__(self):
-        return len(self.img_paths)
-
-    def __getitem__(self, idx):
-        img_path = self.img_paths[idx]
-        img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB).astype(np.uint8)
-        img = self.transform(img, self.phase)
-
-        return img
-
-
-# Monet Dataset ---------------------------------------------------------------------------
-class CycleGanDataset(Dataset):
+# CycleGAN Dataset ---------------------------------------------------------------------------
+class CycleGANDataset(Dataset):
     def __init__(self, base_img_paths, style_img_paths, transform, phase='train'):
         self.base_img_paths = base_img_paths
         self.style_img_paths = style_img_paths
