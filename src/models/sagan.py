bd60491798ffd3c42a0eeb30e3ba9d9c095555d0
navitacion <masataka.i.navi@gmail.com>
2021-04-27 22:19:14 +0900

    modify:network

--- src/models/sagan.py
+++ src/models/sagan.py
@@ -6,31 +6,25 @@ from torch import nn
 class Self_Attention(nn.Module):
     def __init__(self, in_dim):
         super(Self_Attention, self).__init__()
-        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
-        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 8, kernel_size=1)
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 16, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim // 16, kernel_size=1)
         self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
-
         self.softmax = nn.Softmax(dim=-2)
-
         self.gamma = nn.Parameter(torch.zeros(1))
 
     def forward(self, x):
-        X = x
+        proj_query = self.query_conv(x).view(x.shape[0], -1, x.shape[2] * x.shape[3])
+        proj_key = self.key_conv(x).view(x.shape[0], -1, x.shape[2] * x.shape[3])
+        proj_value = self.value_conv(x).view(x.shape[0], -1, x.shape[2] * x.shape[3])
 
-        proj_query = self.query_conv(X).view(X.shape[0], -1, X.shape[2] * X.shape[3])
-        proj_key = self.key_conv(X).view(X.shape[0], -1, X.shape[2] * X.shape[3])
-        proj_value = self.value_conv(X).view(X.shape[0], -1, X.shape[2] * X.shape[3])
-        proj_query = proj_query.permute(0, 2, 1)
+        S = torch.bmm(proj_query.permute(0, 2, 1), proj_key)
+        del proj_query, proj_key
 
-        S = torch.bmm(proj_query, proj_key)
+        o = torch.bmm(proj_value, self.softmax(S)).view(x.shape[0], x.shape[1], x.shape[2], x.shape[3])
+        x = x + self.gamma * o
+        del proj_value, S
 
-        attention_map = self.softmax(S).permute(0, 2, 1)
-
-        o = torch.bmm(proj_value, attention_map.permute(0, 2, 1))
-        o = o.view(X.shape[0], X.shape[1], X.shape[2], X.shape[3])
-        out = x + self.gamma * o
-
-        return out
+        return x
 
 
 class SA_ConvBatchRelu(nn.Module):
@@ -67,12 +61,13 @@ class SAGAN_Discriminator(nn.Module):
 
         self.block = nn.Sequential(
             SA_ConvBatchRelu(3, filter * 1, kernel_size=4, stride=2, padding=1),
+            SA_ConvBatchRelu(filter * 1, filter * 1, kernel_size=4, stride=2, padding=1),
             SA_ConvBatchRelu(filter * 1, filter * 2, kernel_size=4, stride=2, padding=1),
             SA_ConvBatchRelu(filter * 2, filter * 4, kernel_size=4, stride=2, padding=1),
             Self_Attention(in_dim=filter * 4),
             SA_ConvBatchRelu(filter * 4, filter * 4, kernel_size=4, stride=2, padding=1),
             SA_ConvBatchRelu(filter * 4, filter * 8, kernel_size=4, stride=2, padding=1),
-            Self_Attention(in_dim=filter * 8),
+            # Self_Attention(in_dim=filter * 8),
         )
 
         self.last = nn.utils.spectral_norm(nn.Conv2d(filter * 8, 1, kernel_size=4, stride=1))
@@ -95,7 +90,8 @@ class SAGAN_Generator(nn.Module):
             Self_Attention(in_dim=filter * 4),
             SA_UpsampleConvBatchRelu(filter * 4, filter * 2, stride=2, padding=1),
             SA_UpsampleConvBatchRelu(filter * 2, filter * 1, stride=2, padding=1),
-            Self_Attention(in_dim=filter)
+            SA_UpsampleConvBatchRelu(filter * 1, filter * 1, stride=2, padding=1),
+            # Self_Attention(in_dim=filter)
         )
 
         self.last = nn.Sequential(
